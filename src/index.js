41ce1e64bac2eb87d1555666daf5ca8fad7d97c1
LucaPalminteri <lucapalminteri02@gmail.com>
2022-11-27 12:25:46 -0300

    feat: add about us

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-const app = require('express')();
+const express = require('express')
+const app = express();
 
 app.get('/', (req, res ) => {
-    res.sendFile('./index.html', {root: __dirname })
-} 
-);
+    res.sendFile('./index.html', {root: __dirname})
+});
+
+app.get('/about-us', (req,res) => {
+    res.sendFile('./about-us.html', {root: __dirname})
+})
 
 const port = process.env.PORT || 8080;
 
